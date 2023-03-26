7223e7b58b8dcaebb6e70150f79077ad037b7e7d
Majed Embaya <web@embaya.fr>
2023-03-26 22:53:28 +0200

    app logic implementation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dictionary
+It is an application in go language that allows to write a dictionary of words definition
+
+# How to use
+```
+./dict -action "<action_name>" "<word>" "<definition-for-add-action>
+```
+
+- action_name : ```<list>/<define>/<add>/<remove>```
+
+# Features to be added
+- add a tag by definition and display the lists of definitions associated with this tag
+- sort by date / tag
+- Show Statistics :
+  - by used letters
+  - average length of words / defintions
--- main.go
+++ main.go
@@ -2,21 +2,65 @@ package main
 
 import (
 	dic "dictionary/dictionary"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	action := flag.String("action", "list", "action to perform n dectionary")
+
 	d, err := dic.New("./badger")
 	handleErr(err)
 	defer d.Close()
 
+	flag.Parse()
+
+	switch *action {
+	case "list":
+		actionList(d)
+	case "add":
+		actionAdd(d, flag.Args())
+	case "define":
+		actionDefine(d, flag.Args())
+	case "remove":
+		actionRemove(d, flag.Args())
+	default:
+		fmt.Printf("unknown action : %v\n", *action)
+	}
+
 	d.Add("php", "is a good langage")
-	words, entries, _ := d.List()
+
+}
+
+func actionRemove(d *dic.Dictionary, args []string) {
+	word := args[0]
+	err := d.Remove(word)
+	handleErr(err)
+	fmt.Printf("'%v' was removed from the dictionary\n", word)
+}
+
+func actionDefine(d *dic.Dictionary, args []string) {
+	word := args[0]
+	entry, err := d.Get(word)
+	handleErr(err)
+	fmt.Println(entry)
+}
+
+func actionAdd(d *dic.Dictionary, args []string) {
+	word := args[0]
+	defintion := args[1]
+	err := d.Add(word, defintion)
+	handleErr(err)
+	fmt.Printf("%v added to dictionary\n", word)
+}
+
+func actionList(d *dic.Dictionary) {
+	words, entries, err := d.List()
+	handleErr(err)
 	for _, word := range words {
 		fmt.Println(entries[word])
 	}
-
 }
 
 func handleErr(err error) {
